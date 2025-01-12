93bc6293c63729a7c3cf3d04e9401d1d31e7dd54
tabularasa066a <tabularasa066a@gmail.com>
2025-01-12 21:38:33 +0900

    学習済みモデル等を削除するシェルを追加

---
+++ reset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# models 配下の .pth ファイルをすべて削除
+find ./models -type f -name "*.pth" -exec rm -f {} +
+
+# models/embeddings 配下の .idx ファイルをすべて削除
+find ./models/embeddings -type f -name "*.idx" -exec rm -f {} +
